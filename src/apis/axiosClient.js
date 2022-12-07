2f2ef23ce422cfc4ca34b45516ef82a9fbb39f44
hiepnd <hiepnd@vnpt-technology.com>
2022-12-07 18:51:39 +0700

    setup project

---
+++ src/apis/axiosClient.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export const axiosClient = axios.create({
+    baseURL: import.meta.env.REACT_APP_API_URL,
+    headers: {
+        'content-type': 'application/json',
+    },
+});
+
+axiosClient.interceptors.request.use(async (config) => {
+// Handle token here ...
+    return config;
+})
+axiosClient.interceptors.response.use((response) => {
+    if (response && response.data) {
+        return response.data;
+    }
+    return response;
+}, (error) => {
+// Handle errors
+    throw error;
+});
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import {QueryClient, QueryClientProvider} from "react-query";
 import ReactDOM from 'react-dom/client'
+import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
 import App from './App'
 import './index.css'
 
+const store = {}
+const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <Provider store={store}>
+          <BrowserRouter>
+              <QueryClientProvider client={queryClient}>
+                  <App />
+              </QueryClientProvider>
+          </BrowserRouter>
+      </Provider>
   </React.StrictMode>
 )
